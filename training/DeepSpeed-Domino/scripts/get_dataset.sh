731877584b57f6a7b17c5db60c286369bb287186
FFGGSSJJ <fuguan@student.ethz.ch>
2024-11-17 18:34:29 +0100

    mv data scripts

---
+++ training/DeepSpeed-Domino/scripts/get_dataset.sh
@@ -0,0 +1,4 @@
+wget https://huggingface.co/bigscience/misc-test-data/resolve/main/stas/oscar-1GB.jsonl.xz
+wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-vocab.json
+wget https://s3.amazonaws.com/models.huggingface.co/bert/gpt2-merges.txt
+xz -d oscar-1GB.jsonl.xz
---
+++ training/DeepSpeed-Domino/scripts/process_dataset.sh
@@ -0,0 +1,9 @@
+python tools/preprocess_data.py \
+    --input oscar-1GB.jsonl \
+    --output-prefix my-gpt2 \
+    --vocab-file gpt2-vocab.json \
+    --dataset-impl mmap \
+    --tokenizer-type GPT2BPETokenizer \
+    --merge-file gpt2-merges.txt \
+    --append-eod \
+    --workers 8
